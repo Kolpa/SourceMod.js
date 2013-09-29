f6a4786eda1e423f331a05c1823bba660a270087
Matheus Valadares <matheusavs3@gmail.com>
2013-09-29 15:04:16 -0300

    Fixed require

--- server/sourcemod.js/sm.js
+++ server/sourcemod.js/sm.js
@@ -1,7 +1,7 @@
 (function(){
 	require = function(file){
 		if(require.cache.hasOwnProperty(file)) return require.cache[file];
-		__require(file);
+		return __require(file);
 	}
 	
 	require.cache = {};
